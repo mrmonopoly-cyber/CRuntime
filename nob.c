3044fd09549b0ce37ed5aab9ae8c556dbeddf5cb
Alberto Damo <alberto.damo@gmail.com>
2026-03-09 15:10:17 +0100

    fix(nob): fixed nob script, now recursively goes into directory to compile the files

--- nob.c
+++ nob.c
@@ -15,38 +15,14 @@
 
 const char* root = NULL;
 
-static int _build_obj(const char* base_src_dir)
+static bool _compile_files(Walk_Entry entry)
 {
-  int result=0;
-  Cmd cmd = {0};
-  Dir_Entry dir_entry = {0};
-  char* src_cursor = NULL;
-  char src_temp_path[PATH_MAX] = {0};
+  bool result = true;
+  Cmd cmd ={0};
 
-  strncat(src_temp_path, base_src_dir, strlen(base_src_dir));
-  src_cursor = src_temp_path + strlen(base_src_dir);
-  (*src_cursor++) = '/';
-
-  if(!dir_entry_open(base_src_dir, &dir_entry))
+  if (entry.type == NOB_FILE_REGULAR && strncmp(entry.path + strlen(entry.path)-2, ".h", 2))
   {
-    result =1;
-    goto end;
-  }
-
-  while (dir_entry_next(&dir_entry)){
-    char* temp_cursor = src_cursor;
-    const int dir_name_length = strlen(dir_entry.name);
-    if (
-        !strncmp(dir_entry.name, "..", dir_name_length) || 
-        !strncmp(dir_entry.name, ".", dir_name_length) ||
-        !strncmp(dir_entry.name + dir_name_length-2, ".h", 2)
-        )
-    {
-      continue;
-    }
-
-    snprintf(temp_cursor, strlen(dir_entry.name) + 1, "%s", dir_entry.name);
-    nob_log(INFO, "working on %s", dir_entry.name);
+    nob_log(INFO, "compiling: working on %s",entry.path);
 
     cmd_append(&cmd, "cc");
     cmd_append(&cmd, "-Wall");
@@ -55,59 +31,94 @@ static int _build_obj(const char* base_src_dir)
     cmd_append(&cmd, "-pedantic");
     cmd_append(&cmd, "-I../lib/CResult");
     cmd_append(&cmd, "-I../lib/CVector");
+    cmd_append(&cmd, "-I../src/common");
     cmd_append(&cmd, "-c");
-    cmd_append(&cmd, src_temp_path);
+    cmd_append(&cmd, entry.path);
 
-    if (!cmd_run(&cmd, .dont_reset = false)){
-      result = 3;
-      goto clean_dir;
-    };
-  };
+    if (!cmd_run(&cmd, .dont_reset = false)) nob_return_defer(false);
+    result = true;
+  }
+  else
+  {
+    nob_log(INFO, "skipping %s",entry.path);
+  }
 
-clean_dir:
-  dir_entry_close(dir_entry);
+defer:
   cmd_free(cmd);
-end:
   return result;
 }
 
-static int _link_obj(const char* o_file)
+typedef struct{
+  Cmd* p_cmd;
+  char* buffer;
+  int size;
+  size_t available_space;
+  const char* o_file;
+}FindObjFileInput;
+
+static bool _find_obj_file(Walk_Entry entry)
+{
+  bool result = true;
+  Nob_String_View sv = sv_from_cstr(entry.path);
+  FindObjFileInput* const input =  (FindObjFileInput*) entry.data;
+  const size_t space_needed = sv.count + 2;
+
+  if (entry.type == NOB_FILE_REGULAR && 
+      !strncmp(entry.path + sv.count - 2, ".o", 2))
+  {
+    if (input->available_space < space_needed){
+      input->buffer = (char*) NOB_REALLOC(input->buffer, input->size + space_needed);
+      memset(input->buffer + input->size, 0, space_needed);
+      if (!input->buffer){
+        nob_log(ERROR, "realloc failed, by more ram");
+        return false;
+      }
+      input->size += space_needed;
+      input->available_space = space_needed;
+    }
+    strncat(input->buffer, sv.data, input->available_space);
+    strncat(input->buffer, " ", input->available_space);
+    input->available_space-=(space_needed);
+  }
+
+  return result;
+}
+
+
+static int _link_obj(const char* build_dir, const char* o_file)
 {
   int result=0;
-  Dir_Entry dir_entry = {0};
   Cmd cmd = {0};
+  FindObjFileInput input ={
+    .p_cmd = &cmd,
+    .buffer = NULL,
+    .size = 0,
+    .available_space =0,
+    .o_file = o_file,
+  };
 
   cmd_append(&cmd, "cc");
   cmd_append(&cmd, "-Wall");
   cmd_append(&cmd, "-Wextra");
 
-  if(!dir_entry_open(".", &dir_entry)){
-    result =1;
-    goto clean_cmd;
-  }
-  while (dir_entry_next(&dir_entry))
-  {
-    if (!strncmp(dir_entry.name, "..", 2) || 
-        !strncmp(dir_entry.name, ".", 1) ||
-        !strncmp(dir_entry.name, o_file, strlen(dir_entry.name)))
-    {
-      nob_log(INFO, "skipping %s", dir_entry.name);
-      continue;
-    }
+  if(!walk_dir(build_dir, _find_obj_file, .data = &input)) return_defer(1);
 
-    nob_log(INFO, "working on: %s", dir_entry.name);
-    cmd_append(&cmd,dir_entry.name);
-  };
+  for(char* cursor=strtok(input.buffer, " ");cursor;cursor = strtok(NULL, " "))
+  {
+    nob_log(INFO, "file to link: %s", cursor);
+    cmd_append(&cmd, cursor);
+  }
 
   cmd_append(&cmd, "-o");
   cmd_append(&cmd, o_file);
 
-  if (!cmd_run(&cmd, .dont_reset = false)){
-    result = 4;
-  }
+  if (!cmd_run(&cmd,0)) return_defer(2);
 
-  dir_entry_close(dir_entry);
-clean_cmd:
+defer:
+  if(input.buffer){
+    free(input.buffer);
+    input.buffer = NULL;
+  }
   cmd_free(cmd);
   return result;
 }
@@ -118,6 +129,7 @@ int main(int argc, char** argv)
   int err =0;
 
   char src_dir[PATH_MAX] = {0};
+  char build_dir[PATH_MAX] = {0};
 
   root = get_current_dir_temp();
   const int pwd_length = strlen(root);
@@ -126,16 +138,21 @@ int main(int argc, char** argv)
   src_dir[pwd_length] = '/';
   strncat(src_dir, SRC_DIR, PATH_MAX - sizeof(SRC_DIR));
 
+  strncat(build_dir, root, pwd_length);
+  build_dir[pwd_length] = '/';
+  strncat(build_dir, BUILD_DIR, PATH_MAX - sizeof(BUILD_DIR));
+
   if(!mkdir_if_not_exists(BUILD_DIR)) return 1;
   if(!set_current_dir(BUILD_DIR)) return 2;
 
   nob_log(INFO, "src dir: %s", src_dir);
-  nob_log(INFO, "build dir: %s/%s", root,BUILD_DIR);
+  nob_log(INFO, "build dir: %s", build_dir);
 
   nob_log(INFO, "building objs");
-  if((err = _build_obj(src_dir))) return(err);
+  if(!walk_dir(src_dir,_compile_files,0)) return err;;
+
   nob_log(INFO, "linking objs");
-  if((err = _link_obj(O_FILE))) return(err);
+  if((err = _link_obj(build_dir, O_FILE))) return(err);
 
   if(!set_current_dir(root)) return(2);
 
