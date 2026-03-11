846bb63b73b1395e1ce918afbbebd9a69758ca01
Alberto Damo <alberto.damo@gmail.com>
2026-03-11 17:10:13 +0100

    feat(extern_lib): added interface to expose glibc library used in the runtime. Useful for freestanding environment

---
+++ src/CRuntime/common/HAL/extern_lib.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//Copyright (c) 2026 Alberto Damo. All Rights Reserved.
+
+/**
+ * \file extern_lib.h
+ * \brief interface with all the function of the glibc used in the project.
+ * \brief if you are in a freestanding env you need to implement the following function
+ *
+ * \author Alberto Damo
+ * \date 2026
+ */
+
+#include <stddef.h>
+
+void *memset(void *s, int c, size_t n);
