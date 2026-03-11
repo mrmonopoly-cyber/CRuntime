2063bd130f8005fae2dbb174f20d699f8701c9d2
Alberto Damo <alberto.damo@gmail.com>
2026-03-11 17:09:21 +0100

    feat(errors): added extra errors and FROM macro error conversion

--- src/CRuntime/common/errors/errors.h
+++ src/CRuntime/common/errors/errors.h
@@ -11,6 +11,9 @@
 
 #define CR_STATUS_OK 0
 #define CR_STATUS_ERR_PROCESS 1
+#define CR_STATUS_ERR_EMPTY_STACK 2
+#define CR_STATUS_ERR_STACK 3
+#define CR_STATUS_ERR_QTASK_EMPTY 4
 
 typedef struct{
   uint16_t status;
@@ -24,3 +27,4 @@ typedef CRESULT_TEMPLATE(bool, CRStatus) CRReturn;
 #define OK() CRESULT_T_OK(CRReturn, 0)
 #define ERR(...) CRESULT_T_ERR(CRReturn, ((CRStatus){__VA_ARGS__}))
 #define TRY(res) do{ if(CRESULT_IS_ERR((res))) return res; }while(0)
+#define FROM(err) ERR(.status = (err).status, .description = (err).description)
